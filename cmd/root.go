64030e2553884be90056eab337f732e92968090e
Paul Gier <pgier@redhat.com>
2017-07-05 09:17:04 -0500

    Remove viper configuration for now
    The viper stuff seems to add about 5MB to the binary size and
    is not currently being used.

--- cmd/root.go
+++ cmd/root.go
@@ -17,9 +17,7 @@ import (
 	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 var cfgFile string
@@ -41,36 +39,6 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	//RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is hawkbuild.yaml)")
 	RootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c",
 		"hawkbuild.yaml", "File containing build configurations")
-
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cobra" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cobra")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
